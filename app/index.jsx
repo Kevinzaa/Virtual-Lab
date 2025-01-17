567d920905b7abba717375e008d6ada5176302e4
taufiqrmadhana <149040722+taufiqrmadhana@users.noreply.github.com>
2025-01-17 13:39:33 +0700

    web layout landing page

--- app/index.jsx
+++ app/index.jsx
@@ -4,6 +4,7 @@ import {
   View,
   StyleSheet,
   ImageBackground,
+  Dimensions
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import theme from "../style";
@@ -12,6 +13,8 @@ import CustomButton from "../components/CustomButton";
 import { Redirect, router } from "expo-router";
 import { useGlobalContext } from "../context/GlobalProvider";
 import Glassmorphism from "../components/Glassmorphism";
+
+const screenWidth = Dimensions.get('window').width;
 
 export default function App() {
   const { isLoading, isLoggedIn } = useGlobalContext();
@@ -29,27 +32,13 @@ export default function App() {
       >
         <View style={styles.glassmorphismContainer}>
           <Glassmorphism>
-            <Text style={styles.text}>
+            <Text style={styles.heading}>
               Unlock the wonders of science with {' '}
-              <Text
-                style={{
-                  color: theme.colors.secondary.DEFAULT,
-                  fontSize: 25,
-                  fontFamily: "Poppins-Bold",
-                }}
-              >
+              <Text style={styles.highlightedText}>
                 SmartLab
               </Text>
             </Text>
-            <Text
-              style={{
-                marginTop: 16,
-                fontSize: 13,
-                fontFamily: "Poppins-Regular",
-                color: "#2A2C41",
-                textAlign: "center",
-              }}
-            >
+            <Text style={styles.subtitle}>
               Where curiosity ignites discovery—step into a world of boundless knowledge with SmartLab.
             </Text>
             <CustomButton
@@ -72,19 +61,46 @@ const styles = StyleSheet.create({
     backgroundColor: theme.colors.primary,
   },
   backgroundImage: {
-    flex: 1, 
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
   glassmorphismContainer: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    paddingHorizontal: 16,
+    padding: screenWidth > 768 ? 40 : 20,
   },
-  text: {
-    fontSize: 25,
+  heading: {
+    fontSize: screenWidth > 768 ? 36 : 25,
     color: "#2A2C41",
     fontFamily: "Poppins-Bold",
     textAlign: "center",
-    marginTop: 20,
+    marginBottom: 20,
   },
+  highlightedText: {
+    color: theme.colors.secondary.DEFAULT,
+    fontFamily: "Poppins-Bold",
+  },
+  subtitle: {
+    fontSize: screenWidth > 768 ? 18 : 13,
+    fontFamily: "Poppins-Regular",
+    color: "#2A2C41",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    width: screenWidth > 768 ? '50%' : '80%',
+    marginTop: 20,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 50,
+    backgroundColor: theme.colors.secondary.DEFAULT,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 18,
+    color: 'white',
+    fontFamily: "Poppins-Bold",
+  }
 });
